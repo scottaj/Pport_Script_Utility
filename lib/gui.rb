cf3cbf83510e10e0c6a1258ca25de496e5a0acb3
scottaj <aljscott1990@gmail.com>
2010-07-23 14:53:37 -0600

    Removed unused require

--- lib/gui.rb
+++ lib/gui.rb
@@ -11,7 +11,6 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-require 'logger'
 require 'script'
 
 # Module containing all non class specific code, constants, globals,
